3c98e4743abb3e9edb031fc2674ca1c87d8655f6
aditibanerji <aditi.banerji@yahoo.com>
2023-07-28 23:48:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import './App.css';
 import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
      <Nav></Nav>
+    
   
     </div>
   );
