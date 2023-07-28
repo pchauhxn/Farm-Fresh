582861697196eb51a376fa0e97f5381a3a5803ab
aditibanerji <aditi.banerji@yahoo.com>
2023-07-28 23:54:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import './App.css';
 import Nav from './components/Nav';
-<<<<<<< HEAD
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,16 +10,7 @@ function App() {
      <Nav></Nav>
     
   
-=======
-import Login from "./components/Login";
-import Register from "./components/Register";
-function App() {
-  return (
-    <div className="App">
-   <Nav></Nav>
-   <Login></Login>
-    <Register></Register>
->>>>>>> cf4bee5e902ae2e563fc23e813a498a3e6c71d20
+
     </div>
   );
 }
